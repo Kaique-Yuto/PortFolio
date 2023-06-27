cd6fe6300b82868d03876bca068badde1141176c
Kaique Yuto <kaiqueyuto1@gmail.com>
2023-06-26 23:45:57 -0300

    Adicionar arquivos ao repositório PortFolio

---
+++ PortFolio/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+      slidesToShow: 1, // Número de imagens exibidas por slide
+      autoplay: true, // Reprodução automática
+      autoplaySpeed: 2000, // Velocidade da animação em milissegundos
+      arrows: true, // Exibir setas de navegação
+        dots: true,
+        adaptiveHeight: true
+    });
+  });
+  
